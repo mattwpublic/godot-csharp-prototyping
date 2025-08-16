d1a4fd4ee06515027d7ba4310c5763d5c089bd2f
mattwpublic <mwideman015@gmail.com>
2025-08-16 11:06:40 -0500

    camera zoom controls

--- scripts/Camera2d.cs
+++ scripts/Camera2d.cs
@@ -5,10 +5,13 @@ public partial class Camera2d : Camera2D
 {
     int DEAD_ZONE = 100;
     float MAX_FOV = 200f;
-    Vector2 OFFSET = new Vector2(60.0f, 0.0f);
-
     float CLOSE_SMOOTHING = 32.0f;
     float FAR_SMOOTHING = 50.0f;
+    float ZOOM_AMOUNT = 1f;
+    float MAX_ZOOM_OUT = -200f;
+    float MAX_ZOOM_IN = 10f;
+    
+    Vector2 OFFSET = new Vector2(60.0f, 0.0f);
 
     AnimationPlayer camera_animator;
     AudioStreamPlayer2D camera_flip_sfx;
@@ -16,6 +19,7 @@ public partial class Camera2d : Camera2D
     
     bool flip_flag_old = true;
     bool flip_flag_new = true;
+    Vector2 new_zoom;
 
     public override void _Ready()
     {
@@ -25,8 +29,9 @@ public partial class Camera2d : Camera2D
         music.Play();
     }
 
-    public override void _Process(double delta) {
-        
+    public override void _Process(double delta)
+    {
+
         base._Process(delta);
 
         if (GetGlobalMousePosition().X - this.GlobalPosition.X > DEAD_ZONE)
@@ -52,13 +57,38 @@ public partial class Camera2d : Camera2D
 
         Node2D player = (Node2D)GetParent();
         Vector2 target_vector = GetGlobalMousePosition() - player.GlobalPosition;
-        if (target_vector.Length() < DEAD_ZONE) {
+        if (target_vector.Length() < DEAD_ZONE)
+        {
             this.PositionSmoothingSpeed = CLOSE_SMOOTHING;
             this.Position = new Vector2(0, 0);
         }
-        else {
+        else
+        {
             this.PositionSmoothingSpeed = FAR_SMOOTHING;
             this.Position = target_vector.Normalized() * Mathf.Clamp((target_vector.Length() - DEAD_ZONE), 0, MAX_FOV) * 0.5f;
+        }
+
+        if (Input.IsActionJustPressed("camera_zoom_in"))
+        {
+            new_zoom.X = this.Zoom.X + ZOOM_AMOUNT;
+            new_zoom.Y = this.Zoom.Y + ZOOM_AMOUNT;
+            if (new_zoom.X > MAX_ZOOM_IN)
+            {
+                new_zoom.X = MAX_ZOOM_IN;
+                new_zoom.Y = MAX_ZOOM_IN;
+            }
+            this.Zoom = new_zoom;
+        }
+        if (Input.IsActionJustPressed("camera_zoom_out"))
+        {
+            new_zoom.X = this.Zoom.X - ZOOM_AMOUNT;
+            new_zoom.Y = this.Zoom.Y - ZOOM_AMOUNT;
+            if (new_zoom.X < MAX_ZOOM_OUT)
+            {
+                new_zoom.X = MAX_ZOOM_OUT;
+                new_zoom.Y = MAX_ZOOM_OUT;
+            }
+            this.Zoom = new_zoom;
         }
     }
 }
