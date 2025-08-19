037d6e386b617fc831ba817a4a8f6460c2f46db3
mattwpublic <mwideman015@gmail.com>
2025-08-19 08:14:22 -0500

    refactored some code for player equipment and started on an animation for shooting

--- scripts/GunContainer.cs
+++ scripts/GunContainer.cs
@@ -1,20 +1,24 @@
 using Godot;
 using System;
 
-public partial class Gun : Sprite2D
+public partial class GunContainer : Node2D
 {
     float fire_rate;
     float time_until_fire = 0f;
     [Export] float rps = 10;
 
     AudioStreamPlayer2D pistol_gunshots;
+    Sprite2D gun;
     Muzzle muzzle;
+    AnimationPlayer anim_player;
 
     public override void _Ready()
     {
         fire_rate = 1 / rps;
-        pistol_gunshots = GetNode<AudioStreamPlayer2D>("Muzzle/PistolGunshotSFX");
-        muzzle = GetNode<Muzzle>("Muzzle");
+        pistol_gunshots = GetNode<AudioStreamPlayer2D>("Gun/Muzzle/PistolGunshotSFX");
+        gun = GetNode<Sprite2D>("Gun");
+        muzzle = GetNode<Muzzle>("Gun/Muzzle");
+        anim_player = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
     public override void _Process(double delta)
@@ -22,14 +26,14 @@ public partial class Gun : Sprite2D
 
         base._Process(delta);
 
-        LookAt(GetGlobalMousePosition());
+        this.LookAt(GetGlobalMousePosition());
         switch (Math.Sign(GlobalPosition.DirectionTo(GetGlobalMousePosition()).X))
         {
             case 1:
-                this.FlipV = false;
+                //gun.FlipV = false;
                 break;
             case -1:
-                this.FlipV = true;
+                //gun.FlipV = true;
                 break;
         }
 
@@ -50,6 +54,7 @@ public partial class Gun : Sprite2D
             muzzle.FireBullet();
             time_until_fire = 0f;
             pistol_gunshots.Play();
+            anim_player.Play("gun_shoot");
         }
     }
 }
